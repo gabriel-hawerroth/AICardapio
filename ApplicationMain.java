5563279ff31c554adb717b58ccef33b231cdff8b
Gabriel Hawerroth <gabriel.hawerroth04@gmail.com>
2023-06-05 22:36:14 -0300

    created DatabaseConnection

---
+++ ApplicationMain.java
@@ -0,0 +1,9 @@
+import methods.DatabaseConnection;
+
+public class ApplicationMain {
+
+    public static void main(String[] args) {
+        DatabaseConnection.conectarBanco();
+    }
+
+}
---
+++ Methods/DatabaseConnection.java
@@ -0,0 +1,31 @@
+package methods;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    static Connection conexao = null;
+
+    public static void conectarBanco() {
+        try {
+            // Carrega a classe do driver JDBC
+            Class.forName("com.mysql.jdbc.Driver");
+            // Conecta ao banco de dados
+            conexao = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/aicardapio", "root", "");
+        } catch (ClassNotFoundException e) {
+            print("Não foi possível carregar o driver JDBC.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            print("Não foi possível conectar ao banco de dados.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void print(String texto) {
+        System.out.println(texto);
+    }
+
+}
